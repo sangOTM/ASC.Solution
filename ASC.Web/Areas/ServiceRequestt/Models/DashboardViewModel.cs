962ddc84e89d2dbdd7ee08558260abd8d5bae3bf
sangOTM <97737368+sangOTM@users.noreply.github.com>
2025-05-17 19:01:23 +0700

    Add files via upload

---
+++ ASC.Web/Areas/ServiceRequestt/Models/DashboardViewModel.cs
@@ -0,0 +1,9 @@
+﻿using ASC.Model.Models;
+
+namespace ASC.Web.Areas.ServiceRequestt.Models
+{
+    public class DashboardViewModel
+    {
+        public List<ServiceRequest> ServiceRequestt { get; set; }
+    }
+}
